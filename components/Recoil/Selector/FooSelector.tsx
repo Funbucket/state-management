007e20a99111f48b2590a12e5994d2237c401066
조해창 <outofchng@gmail.com>
2023-03-23 10:01:53 +0900

    fix

--- components/Recoil/Selector/FooSelector.tsx
+++ components/Recoil/Selector/FooSelector.tsx
@@ -16,7 +16,7 @@ const BarSelector = () => {
     <HStack>
       <VStack border="2px" padding="2" gap={3}>
         <Heading size="lg">이것은 FooSelector컴포넌트입니다.</Heading>
-        <Heading size="md">bar: {foo}</Heading>
+        <Heading size="md">foo: {foo}</Heading>
         <Button onClick={() => setFoo("p")}>chageBar</Button>
         <Text>{renderCount}번 렌더링되었습니다.</Text>
       </VStack>
